d352c14d216e65864830a673964a709f20eda492
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-01-31 14:43:05 +0000

    JavaScript
    Typing header and moving gradient

---
+++ javaScript/main.js
@@ -0,0 +1,28 @@
+//Banner text
+//const timeline = gsap.timeline({repeat: -1});
+const headerElement = document.querySelector("#header1");
+const subHeaderElement = document.querySelector("#header2")
+
+console.log(headerElement.classList);
+function typingEffect(element, speed)
+{
+    const text = element.innerText.split("");
+
+    element.innerText = "";
+
+    text.forEach((character, index) => {
+        setTimeout(() => { // loop is repeating whatever the speed variable is set to.
+            element.innerHTML += character; // adds each character from the element one at a time to the inner HTML.
+        }, index * speed);
+        
+    });
+}
+
+typingEffect(headerElement, 200);
+
+if (subHeaderElement !== null) // some pages don't have the h2 on banner, so this checks if it does on each page (otherwise it will throw null errors)
+{
+    typingEffect(subHeaderElement, 200);
+}
+
+
