9d84ff7bd21db91aee5965f844033f00857c42ff
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-05 13:31:51 +0000

    changes

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,7 +2,7 @@
 const headerElement = document.querySelector("#header1");
 const subHeaderElement = document.querySelector("#header2")
 
-console.log(headerElement.classList);
+console.log(headerElement.classList); //h1
 function typingEffect(element, speed)
 {
     const text = element.innerText.split("");
@@ -11,17 +11,15 @@ function typingEffect(element, speed)
 
     text.forEach((character, index) => {
         setTimeout(() => { // loop is repeating whatever the speed variable is set to.
-            element.innerHTML += character; // adds each character from the element one at a time to the inner HTML.
+            element.innerHTML += character; // adds each character from the element one at a time to the inner header text.
         }, index * speed);
         
     });
 }
 
-typingEffect(headerElement, 200);
+typingEffect(headerElement, 200); 
 
-if (subHeaderElement !== null) // some pages don't have the h2 on banner, so this checks if it does on each page (otherwise it will throw null errors)
+if (subHeaderElement !== null) //for the h2//
 {
     typingEffect(subHeaderElement, 200);
 }
-//Hamburger Menu//
-
