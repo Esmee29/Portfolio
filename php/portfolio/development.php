339bff3810f51b7c06dec800c334cd9b264670ca
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-07-13 12:10:52 +0100

    design

---
+++ php/portfolio/development.php
@@ -0,0 +1,112 @@
+<section id="projects" class="projects">
+    <div class="index-title">
+        <h2 class="h1">Projects</h2>
+    </div>
+
+    <div class="project-grid">
+        <div class="card">
+            <img src="Images/Netmatters.webp" class="image-card" alt="Netmatters homepage project">
+            <div class="card__content">
+                <p class="card__title">Netmatters Homepage</p>
+                <p class="card__description">A replica of the Netmatters homepage created as part of the Scion Coalition Scheme. The website was meticulously crafted using HTML, Sass/CSS, JavaScript, and PHP.</p>
+                <div class="card__buttons">
+                    <a href="https://netmatters.esmee-fulcher.netmatters-scs.co.uk/" target="_blank"><button class="card__button">View Project</button></a>
+                    <a href="https://github.com/Esmee29/Netmatters-PHP" target="_blank"><button class="card__button secondary">View Repository</button></a>
+                </div>
+                <h4 class="tools-title">Technologies</h4>
+                <ul class="projects-block-tools">
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-html5-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-css3-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-sass-original symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-javascript-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-php-plain php"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <img src="Images/contact-form.png" class="image-card" alt="Netmatters contact page project">
+            <div class="card__content">
+                <p class="card__title">Netmatters Contact page</p>
+                <p class="card__description">This is a recreated Netmatters contact page, part of the Scion Coalition Scheme. It integrates HTML, Sass/CSS, JavaScript, and PHP to include a contact form that sends data to a database upon submission.</p>
+                <div class="card__buttons">
+                    <a href="https://netmatters.esmee-fulcher.netmatters-scs.co.uk/contact-us.php" target="_blank"><button class="card__button">View Project</button></a>
+                    <a href="https://github.com/Esmee29/Netmatters-PHP" target="_blank"><button class="card__button secondary">View Repository</button></a>
+                </div>
+                <h4 class="tools-title">Technologies</h4>
+                <ul class="projects-block-tools">
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-html5-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-css3-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-sass-original symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-javascript-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-php-plain php"></i>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <img src="Images/JS-Array.webp" class="image-card" alt="JavaScript-Array project">
+            <div class="card__content">
+                <p class="card__title">JavaScript Array</p>
+                <p class="card__description">As part of the Scion Coalition Scheme assessment with Netmatters,
+                    I created an Image-to-Email Assigner that allows users to input their email addresses and allocate images from an image API directly to their email accounts.</p>
+                <div class="card__buttons">
+                    <a href="https://esmee-fulcher.netmatters-scs.co.uk/Array/" target="_blank"><button class="card__button">View Project</button></a>
+                    <a href="https://github.com/Esmee29/JavaScript-Array" target="_blank"><button class="card__button secondary">View Repository</button></a>
+                </div>
+                <h4 class="tools-title">Technologies</h4>
+                <ul class="projects-block-tools">
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-html5-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-css3-plain symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-sass-original symbol"></i>
+                        </div>
+                    <li>
+                        <div class="icon">
+                            <i class="devicon-javascript-plain symbol"></i>
+                    </li>
+            </div>
+            </ul>
+        </div>
+
+    </div>
+
+
+    </div>
